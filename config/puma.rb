a4229b11f0231fc7658cee6fbd3f727f2df7d992
kisshan62 <kishimotok62@yahoo.co.jp>
2022-04-20 22:28:09 +0900

    circleci_test5

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
 rails_env = ENV.fetch("RAILS_ENV") { "development" }
 if rails_env == "production"
-  stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+  # stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 else
   port ENV.fetch("PORT") { 3000 }
 end

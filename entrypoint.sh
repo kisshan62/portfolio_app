0d791c151c5b938bf8bb57a8d3c731901d0d1111
kisshan62 <kishimotok62@yahoo.co.jp>
2022-11-29 22:56:49 +0900

    update_entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ rm -f /app/tmp/pids/server.pid
 # bundle exec rails db:create
 # bundle exec rails db:migrate
 # bundle exec rails db:seed
-bundle exec rails db:reset
+bundle exec RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
